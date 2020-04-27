7ff77b4966e425458c72cd3992038d8238edd10b
motunikomi <hasimoto910@gmail.com>
2020-04-28 01:47:29 +0900

    test追加

---
+++ bin/test/com/home/HomePageGradleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.home;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomePageGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	 public void 必ず成功() throws Exception {
+	        Assertions.assertEquals(1,1);
+	  }
+
+}
